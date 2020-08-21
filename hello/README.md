ddb2bb70db44156269fc2238db1eeeca240835a9
Ellinor Leander <el222nj@student.lnu.se>
2020-08-21 18:14:40 +0200

    updated files

---
+++ hello/README.md
@@ -0,0 +1,4 @@
+This is a README file
+======================
+
+Describe project so all can download it and install. 
---
+++ hello/hello.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello world</title>
+</head>
+<body>
+    <h1>Hello</h3>
+    <? php echo "My name is Cirrus"; ?>
+</body>
+</html>
